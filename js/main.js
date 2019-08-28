b5be0d0cbcb161fd848ed11e6858561d36096da3
Jean-Paul Nguyen <jean.paul_nguyen@yahoo.com>
2019-08-28 01:59:33 -0700

    Added animations and a little picture thing

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
 $(document).ready(function () {
     console.log("Hello!");
     console.log("This is a WIP: https://github.com/jp-nguyen/personal-website")
+
+
+    // cycle between pictures
+
+    var next = 1;
+    const numImgs = 4;
+
+    $(".top").click(function() {
+        $(".top").toggleClass("transparent");
+        $(".top").one("transitionend", function() {
+                next = (next + 1) % numImgs;
+                $(next % 2 == 0 ? ".top" : ".bottom").attr("src", "assets/imgs/me" + next + ".jpg");
+            }
+        );
+    });
 });
